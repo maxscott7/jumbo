91894c6cc38fb718b188e18e5576119d0839dc5a
Max <maxwell.scott@umontana.edu>
2022-12-23 20:29:25 -0800

    product screens and routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,24 @@
 import data from "./data";
+import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
-  return (
-    <div>
-      <header>
-        <a href="/">Jumbo Jewelry</a>
-      </header>
-      <main>
-        <h1>Featured Products</h1>
-          <div className="products">
-          {
-              data.products.map((product) => (<div className="product" key={product.slug}>
-                  <a href={`/product/${product.slug}`}>
-                    <img src={product.image} alt={product.name} />
-                  </a>
-                  <div className="product-info">
-                      <a href={`/product/${product.slug}`}>
-                        <p>{product.name}</p>
-                      </a>
-                      <p>${product.price}</p>
-                      <button>Add to cart</button>
-                  </div>
-              </div>))
-          }
-          </div>
-      </main>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <div>
+                <header>
+                    <Link to="/">Jumbo Jewelry</Link>
+                </header>
+                <main>
+                    <Routes>
+                        <Route path="/product/:slug" element={<ProductScreen />} />
+                        <Route path="/" element={<HomeScreen />} />
+                    </Routes>
+                </main>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;

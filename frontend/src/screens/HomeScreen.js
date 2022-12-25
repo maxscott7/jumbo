873bb3f4bf5c319a5857d62c12db7932bb44e6bb
Max <maxwell.scott@umontana.edu>
2022-12-25 01:10:59 -0800

    Functioning card features but not correct size or style

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,8 +1,9 @@
 //import data from "../data";
-import {Link} from "react-router-dom";
 import {useEffect, useReducer, useState} from "react";
 import axios from 'axios';
 import logger from "use-reducer-logger";
+import {Col, Row} from "react-bootstrap";
+import Product from "../components/Product";
 
 const reducer = (state, action) => {
     switch (action.type) {
@@ -38,21 +39,15 @@ function HomeScreen() {
     return <div><h1>Featured Products</h1>
         <div className="products">
             {
-                loading ? (<div>Loading...</div>):
-                    error ? (<div>{error}</div>):
-                        products.map((product) => (<div className="product" key={product.slug}>
-                            <Link to={`/product/${product.slug}`}>
-                                <img src={product.image} alt={product.name}/>
-                            </Link>
-                            <div className="product-info">
-                                <Link to={`/product/${product.slug}`}>
-                                    <p>{product.name}</p>
-                                </Link>
-                                <p>${product.price}</p>
-                                <button>Add to cart</button>
-                            </div>
-                        </div>))
-            }
+                loading ? (<div>Loading...</div>) :
+                    error ? (<div>{error}</div>) : (
+                        <Row>
+                            {products.map((product) => (
+                                <Col key={product.slug} sm={6} md={4} lg={3} className="mb-3">
+                                    <Product product={product}></Product>
+                                </Col>))}
+                        </Row>
+                    )}
         </div>
     </div>;
 }
